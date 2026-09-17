53c764bd1769cfc1450bb7a264c94ba985186036
joqd <gorgdis@gmail.com>
2026-09-17 17:25:50 +0330

    fix: update created_at field to use timezone-aware datetime

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from sqlmodel import Field, SQLModel
 
@@ -9,4 +9,4 @@ class User(SQLModel, table=True):
     username: str | None = None
     first_name: str | None = None
     last_name: str | None = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=datetime.now(timezone.utc))
